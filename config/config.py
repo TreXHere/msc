5264e50c6d9f7e8603609759d6668c4dfed6dd56
TreXHere <105743848+TreXHere@users.noreply.github.com>
2024-08-17 06:41:54 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,15 +1,15 @@
 import os
 
 
-API_ID: int = int(os.getenv("API_ID", 0))
+API_ID: int = int(os.getenv("API_ID", "7239888"))
 
-API_HASH: str = os.getenv("API_HASH", "")
+API_HASH: str = os.getenv("API_HASH", "7101eb117745b360c0edc55c41358776")
 
-SESSION_STRING: str = os.getenv("SESSION_STRING", "")
+SESSION_STRING: str = os.getenv("SESSION_STRING", "BQBueNAArcb_hcfiUSwGf0zOl3XKY0tvYzf29gypv_fUDkIbQ2yGQHtDM0sZ526ftwtfuv41V9UCyN-AUnmfG9777jMwmUXIKzXsihZ6pk_kVNy_s3qwTSGgacBeeS8wVERkTj5RMRlCf5PwAPTpTMbjiW8EkqGil3b181VHMrFfv33D8Krli_L3zAIx887Uam2KsQpZVKq3x5CZAkvUjUn5dzKYY2Mf2NFFcNS7ED5hP5i8DDPwegeWJ1afxsCJUklf0Ohx39gdwKibzlJon0fiE4GS7hM0LoIOA2Q8IQwEYirboO_gYMUX8HhKwEHRKwEcLIPQ89BlLwaI1yMYVl9COE4FGwAAAAGyPJBQAA")
 
-OWNER_ID: list[int] = [int(os.getenv("OWNER_ID", 0))]
+OWNER_ID: list[int] = [int(os.getenv("OWNER_ID", "7474825167"))]
 
-LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID", 0))
+LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID", "-1002208145270"))
 
 PREFIX: str = str(".")
 
